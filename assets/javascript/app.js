7ec95041b3c3399c9f9eb2ea51559eded03f3a48
Jeremy Chiu <jeremybarotchiu@gmail.com>
2019-07-23 16:06:41 +1000

    added code for queryUrl and ajax

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,20 @@
 $(document).ready(function () {
 
+    $(".animal-btn").on("click", function () {
 
+        var queryUrl = ""
+
+        $.ajax({
+                url = queryUrl,
+                method = 'GET'
+            })
+            .then(function (response) {
+
+            });
+
+
+
+    });
 
 
 

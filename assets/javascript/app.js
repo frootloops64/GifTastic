dff1f983a719a11003758e8ae9f5147063cc3fa3
Jeremy Chiu <jeremybarotchiu@gmail.com>
2019-07-26 23:50:29 +1000

    add js code for buttons and gif urls

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,82 @@
 $(document).ready(function () {
 
-    $("#submit-btn").on("click", function () {
+    var startUrl = "https://api.giphy.com/v1/gifs/search?";
+    var apiKey = "api_key=jr4aVK0FtI0qBftQR9M21U0ixSWkXggu&q=";
+    var initialUrl = startUrl + apiKey;
 
-        var query = $('#animal').val().trim();
-        console.log(query);
 
-        var startUrl = "https://api.giphy.com/v1/gifs/search?";
-        var apiKey = "api_key=jr4aVK0FtI0qBftQR9M21U0ixSWkXggu&q=";
+    $(".btn-primary").on("click", function () {
 
-        var queryUrl = startUrl + apiKey + query;
-        console.log(queryUrl);
+        query = $(this).text().toLowerCase();
+        queryUrl = initialUrl + query;
 
         $.ajax({
                 url: queryUrl,
                 method: 'GET'
             })
             .then(function (response) {
+
                 console.log(response);
+
+                for (i = 0; i < 5; i++) {
+
+                    gifUrl = response.data[i].images.downsized.url;
+                    console.log(gifUrl);
+                    var animalUrl = $("<img>");
+                    animalUrl.attr("src", gifUrl);
+                    animalUrl.attr("alt", query + " GIF");
+
+                    $("#gifs-here").append(animalUrl);
+
+                };
+
+
             });
+
+        console.log(query);
+        console.log(queryUrl);
+
+
+
+    });
+
+    // Refer to W6: working-movie-app-solved
+    $("#submit-btn").on("click", function () {
+
+        query = $("#animal").val().trim().toLowerCase();
+        queryUrl = initialUrl + query;
+        console.log(query);
+
+        $.ajax({
+                url: queryUrl,
+                method: 'GET'
+            })
+            .then(function (response) {
+
+                console.log(response);
+
+                for (i = 0; i < 5; i++) {
+
+                    gifUrl = response.data[i].images.downsized.url;
+                    console.log(gifUrl);
+                    var animalUrl = $("<img>");
+                    animalUrl.attr("src", gifUrl);
+                    animalUrl.attr("alt", query + " GIF");
+
+                    $("#gifs-here").append(animalUrl);
+
+                    var animalBtn = $("<button>");
+                    animalBtn.attr("type", "button");
+                    animalBtn.attr("class", "btn btn-primary");
+                    animalBtn.text(query);
+
+                };
+
+
+            });
+
+        console.log(query);
+        console.log(queryUrl);
 
     });
 

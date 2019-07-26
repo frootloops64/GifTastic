165f006ae835f0714f3eee62715b191306726850
Jeremy Chiu <jeremybarotchiu@gmail.com>
2019-07-26 16:16:14 +1000

    got the URL to work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,21 +5,10 @@ $(document).ready(function () {
         var query = $('#animal').val().trim();
         console.log(query);
 
-        function buildQueryURL() {
+        var startUrl = "https://api.giphy.com/v1/gifs/search?";
+        var apiKey = "api_key=jr4aVK0FtI0qBftQR9M21U0ixSWkXggu&q=";
 
-            var startUrl = "api.giphy.com/v1/gifs/search?";
-            var apiKey = "api_key=jr4aVK0FtI0qBftQR9M21U0ixSWkXggu&q=";
-            var ratingClassification = ["G", "PG", "PG-13", "R"];
-            var contentRating = ''
-
-            for (i = 0; i < ratingClassification.length; i++) {
-                contentRating = i;
-            };
-            console.log(contentRating);
-
-        };
-
-        var queryUrl = startUrl + apiKey + query + "&" + contentRating;
+        var queryUrl = startUrl + apiKey + query;
         console.log(queryUrl);
 
         $.ajax({
@@ -27,13 +16,21 @@ $(document).ready(function () {
                 method: 'GET'
             })
             .then(function (response) {
-
+                console.log(response);
             });
-
-
 
     });
 
+
+    // function buildQueryURL() {
+
+    // var ratingClassification = ["G", "PG", "PG-13", "R"];
+    // var contentRating = ''
+
+    // for (i = 0; i < ratingClassification.length; i++) {
+    //     contentRating = i;
+    // };
+    // console.log(contentRating);
 
 
 
